4de75dd113de68c457a2bafc065b7cce3cc85760
ThIsJaCk <y23657689@gmail.com>
2023-08-31 09:26:05 +0800

    更新廠址

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -28,11 +28,10 @@
                     <p>電話 : <a href="tel:03-755-0448">(037) 552 345</a></p>
                     <p>傳真 : <a href="tel:03-755-0448">(037) 550 448</a></p>
                     <p>營業時間 : 08:00-20:00</p>
-                    <p>地址 : 350 苗栗縣竹南鎮國泰路43號</p>
+                    <p>地址 : 350 苗栗縣竹南鎮獅山 102-3 號</p>
                 </div>
                 <div class="col-md-6 contact-map">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3625.419209010048!2d120.8753165148343!3d24.67811208414028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3469b3214f5c27ed%3A0x989c7ff69392efca!2z5bCa6YGU5aGR6Iag5pyJ6ZmQ5YWs5Y-4!5e0!3m2!1szh-TW!2stw!4v1596435467114!5m2!1szh-TW!2stw" width="480" height="360" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
-                    </iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1812.7612655364355!2d120.87126945781849!3d24.674558106965538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3469b3214f5c27ed%3A0x989c7ff69392efca!2z5bCa6YGU5aGR6Iag5pyJ6ZmQ5YWs5Y-4!5e0!3m2!1szh-TW!2stw!4v1693444954935!5m2!1szh-TW!2stw" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
