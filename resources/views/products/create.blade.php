152110b20c8f7c8d104017b236032edd61dc47ce
ThIsJaCk <y23657689@gmail.com>
2023-09-02 18:36:14 +0800

    因應業主需求：商品欄位除了商品編號和名稱之外都取消必填

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -5,7 +5,7 @@
 @endpush
 
 @section('content')
-				
+
 	@component('components.breadcrumbs')
 		<li class="breadcrumb-item">
 			<a href="#">{{ __('Stuffs Management') }}</a>
@@ -17,10 +17,10 @@
 	@endcomponent
 
     <div id="product" class="row justify-content-center">
-        
+
         <span id="getMeterialsName" style="display: none;">{{ route('materials.showName') }}</span>
 		<span id="getMeterialInfo" style="display: none;">{{ route('materials.getInfo') }}</span>
-        
+
         <div class="col-md-10">
             <form id="product_create_form" method="POST" action="{{ route('products.store') }}" enctype="multipart/form-data">
                 @csrf
@@ -32,15 +32,15 @@
                             <label for="shownID">
                                 <span class="text-danger mr-2">*</span>商品編號
                             </label>
-                            <input id="shownID" name="shownID" type="text" class="form-control mb-2 @error('shownID') is-invalid @enderror" value="{{ old('shownID') }}" required autocomplete="off" placeholder="商品編號會自動生產" readonly>
-                            
+                            <input id="shownID" name="shownID" type="text" class="form-control mb-2 @error('shownID') is-invalid @enderror" value="{{ old('shownID') }}" autocomplete="off" placeholder="商品編號會自動生產" readonly>
+
                             <div class="custom-control custom-switch">
                                 <input type="checkbox" class="custom-control-input" name="isManualID" id="ManualID" value="1">
                                 <label class="custom-control-label" for="ManualID">
                                     <small>手動編號</small>
                                 </label>
                             </div>
-                            
+
                             @error('shownID')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -55,8 +55,8 @@
                                 <span class="text-danger mr-2">*</span>商品名稱
                             </label>
 
-                            <input id="name" name="name" type="text" class="form-control mb-2 @error('name') is-invalid @enderror" value="{{ old('name') }}" required autocomplete="off" placeholder="商品名稱預設會自動命名" readonly>
-                            
+                            <input id="name" name="name" type="text" class="form-control mb-2 @error('name') is-invalid @enderror" value="{{ old('name') }}" autocomplete="off" placeholder="商品名稱預設會自動命名" readonly>
+
                             <div class="custom-control custom-switch">
                                 <input type="checkbox" class="custom-control-input" name="isManualNamed" id="ManualNamed" value="1">
                                 <label class="custom-control-label" for="ManualNamed">
@@ -102,9 +102,9 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="specification">
-                                        <span class="text-danger mr-2">*</span>商品規格
+                                        商品規格
                                     </label>
-                                    <input id="specification" name="specification" type="text" class="form-control @error('specification') is-invalid @enderror" value="{{ old('specification') }}" autocomplete="off" required aria-describedby="SpecificationHelp">
+                                    <input id="specification" name="specification" type="text" class="form-control @error('specification') is-invalid @enderror" value="{{ old('specification') }}" autocomplete="off" aria-describedby="SpecificationHelp">
                                     <small id="SpecificationHelp" class="form-text text-muted">塑膠袋的大小（單位：兩、台斤）</small>
                                     @error('specification')
                                         <span class="invalid-feedback" role="alert">
@@ -129,9 +129,9 @@
                         </div>
                         <div class="form-group">
                             <label for="weight">
-                                <span class="text-danger mr-2">*</span>商品重量（單位：兩）
+                                商品重量（單位：兩）
                             </label>
-                            <input id="weight" name="weight" type="text" class="form-control @error('weight') is-invalid @enderror" value="{{ old('weight') }}" autocomplete="off" required placeholder="塑膠袋一包的重量">
+                            <input id="weight" name="weight" type="text" class="form-control @error('weight') is-invalid @enderror" value="{{ old('weight') }}" autocomplete="off" placeholder="塑膠袋一包的重量">
                             @error('weight')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -142,18 +142,17 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="category_id">
-                                        <span class="text-danger mr-2">*</span>
                                         商品類別
                                     </label>
-                
-                                    <select id="category_id" class="form-control @error('category_id') is-invalid @enderror" name="category_id" required>
+
+                                    <select id="category_id" class="form-control @error('category_id') is-invalid @enderror" name="category_id">
                                         @foreach ($categories as $category)
                                             @if($category->id != 2)
                                                 <option value="{{ $category->id }}">{{ $category->name }}</option>
                                             @endif
                                         @endforeach
                                     </select>
-        
+
                                     @error('category_id')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -188,10 +187,10 @@
                             <div class="col-md-4">
                                 <div class="form-group">
                                     <label for="length">
-                                        <span class="text-danger mr-2">*</span>商品長度（公分）
+                                        商品長度（公分）
                                     </label>
-                                    <input id="length" name="length" type="text" class="form-control @error('length') is-invalid @enderror" value="{{ old('length') }}" autocomplete="off" required>
-                                
+                                    <input id="length" name="length" type="text" class="form-control @error('length') is-invalid @enderror" value="{{ old('length') }}" autocomplete="off">
+
                                     @error('length')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -203,10 +202,10 @@
                             <div class="col-md-4">
                                 <div class="form-group">
                                     <label for="width">
-                                        <span class="text-danger mr-2">*</span>商品寬度（公分）
+                                        商品寬度（公分）
                                     </label>
-                                    <input id="width" name="width" type="text" class="form-control @error('width') is-invalid @enderror" value="{{ old('width') }}" autocomplete="off" required>
-                                
+                                    <input id="width" name="width" type="text" class="form-control @error('width') is-invalid @enderror" value="{{ old('width') }}" autocomplete="off">
+
                                     @error('width')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -221,7 +220,7 @@
                                         商品折角（公分）
                                     </label>
                                     <input id="chamfer" name="chamfer" type="text" class="form-control @error('chamfer') is-invalid @enderror" value="{{ old('chamfer') }}" autocomplete="off">
-                                
+
                                     @error('chamfer')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -234,12 +233,11 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="qty_per_pack">
-                                        <span class="text-danger mr-2">*</span>
                                         每件數量
                                     </label>
-                                    
-                                    <input id="qty_per_pack" name="qty_per_pack" type="text" class="form-control @error('qty_per_pack') is-invalid @enderror" value="{{ old('qty_per_pack') }}" required autocomplete="off">
-        
+
+                                    <input id="qty_per_pack" name="qty_per_pack" type="text" class="form-control @error('qty_per_pack') is-invalid @enderror" value="{{ old('qty_per_pack') }}" autocomplete="off">
+
                                     @error('qty_per_pack')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -250,16 +248,15 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="unit">
-                                        <span class="text-danger mr-2">*</span>
                                         慣用單位
                                     </label>
-                                    
-                                    <select id="unit" class="form-control @error('unit') is-invalid @enderror" name="unit" required>
+
+                                    <select id="unit" class="form-control @error('unit') is-invalid @enderror" name="unit">
                                         <option value="package" selected>包</option>
                                         <option value="kg">公斤</option>
                                         <option value="roll">捲</option>
                                     </select>
-        
+
                                     @error('unit')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -272,7 +269,7 @@
                             <label for="intro">
                                 商品簡介
                             </label>
-                            
+
                             <textarea id="intro" name="intro" type="text" class="form-control @error('intro') is-invalid @enderror" value="{{ old('intro') }}"></textarea>
 
                             @error('intro')
@@ -288,12 +285,11 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="quantity">
-                                <span class="text-danger">*</span>
                                 目前庫存量
                             </label>
-    
-                            <input id="quantity" name="quantity" type="text" class="form-control @error('quantity') is-invalid @enderror" value="{{ old('quantity') ?? 0 }}" required>
-                                
+
+                            <input id="quantity" name="quantity" type="text" class="form-control @error('quantity') is-invalid @enderror" value="{{ old('quantity') ?? 0 }}">
+
                             @error('quantity')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -301,16 +297,15 @@
                              @enderror
                         </div>
                     </div>
-                    
+
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="safeQuantity">
-                                <span class="text-danger">*</span>
                                 安全庫存量
                             </label>
-    
-                            <input id="safeQuantity" name="safeQuantity" type="text" class="form-control @error('safeQuantity') is-invalid @enderror" value="{{ old('safeQuantity') ??  0 }}" required>
-                                
+
+                            <input id="safeQuantity" name="safeQuantity" type="text" class="form-control @error('safeQuantity') is-invalid @enderror" value="{{ old('safeQuantity') ??  0 }}">
+
                             @error('safeQuantity')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -326,18 +321,18 @@
                             <label for="comment">
                                 備註
                             </label>
-        
+
                             <textarea id="comment" name="comment" type="text" class="form-control @error('comment') is-invalid @enderror" value="{{ old('comment') }}"></textarea>
-                                
+
                             @error('comment')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
-                            @enderror                 
+                            @enderror
                         </div>
                     </div>
                 </div>
-                
+
                 <hr>
 
                 <product-recipes ref="productRecipes" :materials="materials" v-on:refresh-materials="refreshMaterials"></product-recipes>
@@ -356,5 +351,5 @@
             </form>
         </div>
     </div>
-	
+
 @endsection
